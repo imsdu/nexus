36173eb9e565b71832b76ad727d5b969a8b90f9e
Simon Dumas <simon.dumas@frontiersin.org>
2025-02-25 16:44:26 +0100

    Benchmark multiple projects with docker compose

--- delta/plugins/elasticsearch/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/ElasticSearchPluginModule.scala
+++ delta/plugins/elasticsearch/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/ElasticSearchPluginModule.scala
@@ -178,7 +178,7 @@ class ElasticSearchPluginModule(priority: Int) extends ModuleDef {
         config.batch,
         config.metricsQuery,
         metricIndex,
-        config.indexingEnabled
+        indexingEnabled = false
       )
   }
 
---
+++ tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/kg/ProjectCreateSpec.scala
@@ -0,0 +1,42 @@
+package ch.epfl.bluebrain.nexus.tests.kg
+
+import cats.effect.IO
+import cats.implicits._
+import ch.epfl.bluebrain.nexus.tests.BaseIntegrationSpec
+import ch.epfl.bluebrain.nexus.tests.Identity.listings.Bob
+import ch.epfl.bluebrain.nexus.tests.iam.types.Permission.Organizations
+import ch.epfl.bluebrain.nexus.tests.resources.SimpleResource
+import io.circe.Json
+
+class ProjectCreateSpec extends BaseIntegrationSpec {
+
+  private val org = genId()
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    val setup = for {
+      _ <- aclDsl.addPermission("/", Bob, Organizations.Create)
+      _ <- adminDsl.createOrganization(org, org, Bob)
+    } yield ()
+
+    setup.accepted
+  }
+
+  "Create a list of projects" should {
+    "work" in {
+      (1 to 10_000).toList.traverse { i =>
+        val proj = s"proj$i"
+        for {
+          _ <- IO.println(s"Creating project $proj")
+          _ <- adminDsl.createProjectWithName(org, proj, s"projectName$i", Bob)
+          _ <- (1 to 10).toList.parTraverse { ir =>
+            SimpleResource.sourcePayload(s"http://localhost/$ir", ir).flatMap { payload =>
+              deltaClient.post[Json](s"/resources/$org/$proj/_/", payload, Bob) { expectCreated }
+            }
+          }
+        } yield succeed
+      }
+    }
+  }
+}
